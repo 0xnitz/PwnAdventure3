a0f6ff1dfb7cee017543bf4a37936fd1734c1339
Niv <dindibo4@gmail.com>
2020-09-19 18:29:00 +0300

    Added realtime modification

--- network_proxy.py
+++ network_proxy.py
@@ -1,14 +1,15 @@
 from scapy.all import sniff, TCP, Raw
-from packet import Packet
-
+import packet
+import importlib
 
 def show_pack(pack):
     if pack.haslayer(Raw):
         #print(pack[Raw].load)
         payload = pack[Raw].load
-        p = Packet(payload)
+        p = packet.Packet(payload)
         if len(p.parse_packet()) > 0:
             print(p.parse_packet())
+            importlib.reload(packet)
 
 
 sniff(prn=show_pack, lfilter=lambda x: x.haslayer(TCP) and x[TCP].dport == 3000)
