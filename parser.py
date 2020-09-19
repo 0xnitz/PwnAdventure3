cd969b0dfdd96e553f9d6aa1b3ce0c69b68f8765
prerikoth <nitzan.adut@gmail.com>
2020-09-19 21:30:56 +0300

    Added tp command. ugly code, this is not a dev challenge

--- parser.py
+++ parser.py
@@ -1,22 +1,18 @@
 from scapy.all import sniff, TCP, Raw
 from importlib import reload
-from sys import argv
 
 import packet
 
-targetPort = ''
 
-if len(argv) > 1:
-    targetPort = int(argv[1])
-
-
-def show_pack(pack, port=''):
+def show_pack(pack):
     try:
         if pack.haslayer(Raw) and pack.haslayer(TCP):
             payload = pack[Raw].load
-            p = packet.Packet(payload)
-            if len(p.parse_packet()) > 0:
-                print(p.parse_packet())
+            p = packet.Packet(payload, pack[TCP].sport)
+            if p.is_cmd():
+                print(f'CMD packet -> {p.exec_cmd()}')
+            if len(p.packet_to_str()) > 0:
+                print(p.packet_to_str())
                 reload(packet)
     except Exception as e:
         print(e)
